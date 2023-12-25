009d796e8c15958169394814c3e396b3d07c731a
Lemonpro1 <1208174514@qq.com>
2023-12-25 19:37:41 +0800

    Add files via upload

---
+++ RsetAPI/src/main/java/tasks/RestAPI/demo/ProductManagementApplication.java
@@ -0,0 +1,16 @@
+package tasks.RestAPI.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@SpringBootApplication(scanBasePackages = {"tasks.RestAPI.demo.Mapper"})
+public class ProductManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagementApplication.class, args);
+	}
+
+}
